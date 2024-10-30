66c65a9615e58c4e6616b347b894543eadc1bf45
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-30 11:44:13 +0330

    feature add new category

--- src/js/CategoryView.js
+++ src/js/CategoryView.js
@@ -1,1 +1,37 @@
-import Storage from "./Storage";
+import Storage from "./Storage.js";
+
+const Cattitle = document.querySelector("#category-title");
+const Catdescription = document.querySelector("#category-description");
+const addNewCategory = document.querySelector("#add-new-category");
+
+class CategoryView {
+  constructor() {
+    addNewCategory.addEventListener("click", (e) => this.addNewCategory(e));
+    this.categories = [];
+  }
+  addNewCategory(e) {
+    e.preventDefault();
+    const title = Cattitle.value;
+    const description = Catdescription.value;
+    if (!title || !description) return;
+    Storage.saveCategory({ title, description });
+    this.categories = Storage.getAllCategoryies();
+    //update DOM:
+    this.createdCategoryList();
+  }
+
+  setApp() {
+    this.categories = Storage.getAllCategoryies();
+  }
+
+  createdCategoryList() {
+    let result = `<option class="bg-slate-500 text-slate-300" value="">select a category</option>`;
+    this.categories.forEach((element) => {
+      result += `<option class="bg-slate-500 text-slate-300" value=${element.id}>${element.title}</option>`;
+    });
+    const categoryDOM = document.getElementById("product-category");
+    categoryDOM.innerHTML = result;
+  }
+}
+
+export default new CategoryView();
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
-class App {
-    
-}
+import CategoryView from "./CategoryView.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  CategoryView.setApp();
+  console.log(CategoryView);
+  CategoryView.createdCategoryList();
+});
